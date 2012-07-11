981114539519c6c4824ebb4788efc97bbe75badf
paulbowler <paul_bowler@yahoo.com>
2012-07-11 09:40:22 +0100

    Additional basket test scenario

--- test/cucumber/Basket.feature
+++ test/cucumber/Basket.feature
@@ -7,3 +7,13 @@ Scenario: empty basket
    Given I have an empty basket
     When I view my basket
     Then I see 0 items in my basket
+    
+Scenario: add multiple items to basket
+	Given I have an empty basket
+		And the following items are available:
+  			| sku   | name    | description               | price  |
+			| A1000 | Piano   | A very lovely grand piano | 499900 |
+  			| B2000 | Flute   | A shiny silver flute      | 199500 |
+  			| C3000 | Bassoon | A mellow German bassoon   | 299900 |
+  	When I add product with ID 1 to my basket
+  	Then my basket contains the product with ID 1
--- test/cucumber/steps/BasketSteps.groovy
+++ test/cucumber/steps/BasketSteps.groovy
@@ -1,19 +1,38 @@
 import static cucumber.runtime.groovy.EN.*
+import static groovy.util.GroovyTestCase.assertNotNull
 import cucumber.runtime.PendingException
-
+import cucumber.table.DataTable
 import retail.BasketController
+import retail.Product
 
 BasketController basketController
 def model
+def products
 
 Given(~'^I have an empty basket\$') { ->
     basketController = new BasketController()
 }
 When(~'^I view my basket\$') { ->
-    // Express the Regexp above with the code you wish you had
     model = basketController.show()
 }
 Then(~'^I see (\\d+) items in my basket\$') { int items ->
-	// Express the Regexp above with the code you wish you had
+	assert model.basket.id == 1
 	assert items == (model.basket.basketItems?.count() ?: 0)
+}
+
+Given(~'^the following items are available:\$') { DataTable items ->
+	products = items.asList(Product)
+    products.each { item ->
+    	new Product(sku: item.sku, name: item.name, description: item.description, price: item.price).save(failOnError: true)
+    }
+}
+When(~'^I add product with ID (\\d+) to my basket$') { int id ->
+	def params = [productId: id]
+	basketController.request.params = params
+    basketController.addProductToBasket()
+}
+Then(~'^my basket contains the product with ID (\\d+)\$') { int id ->
+    model = basketController.show()
+    assertNotNull model.basket
+    assert model.basket.total != 0
 }
